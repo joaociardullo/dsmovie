804785d98a662ae7d3f9c76f68ed22e3cd69db5a
joaociardullo <joaociardullo@hotmail.com>
2022-01-11 16:16:13 -0300

    Front end

---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,38 @@
+import Pagination from "../../components/Pagination";
+import MovieCard from "../../components/MovieCard";
+import './style.css';
+
+function Listing() {
+
+    return (
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Listing;
