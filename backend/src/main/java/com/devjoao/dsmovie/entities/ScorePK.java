aa8c1ad6bee7354ba3cc2a7d10e168d70699089f
joaociardullo <joaociardullo@hotmail.com>
2022-01-12 14:33:04 -0300

    adicionando o Seriazable na classe ScorePk

--- backend/src/main/java/com/devjoao/dsmovie/entities/ScorePK.java
+++ backend/src/main/java/com/devjoao/dsmovie/entities/ScorePK.java
@@ -1,10 +1,15 @@
 package com.devjoao.dsmovie.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 @Embeddable
-public class ScorePK {
+public class ScorePK  implements Serializable{
+
+	
+	private static final long serialVersionUID = 1L;
 
 	@ManyToOne
 	@JoinColumn(name = "movie_id")
